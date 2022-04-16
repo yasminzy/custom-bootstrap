ac77d3e592fda2c09606f7692a5056b3b2ded1c6
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2022-04-16 12:01:06 +0800

    style: format file

--- .sassrc.js
+++ .sassrc.js
@@ -1,10 +1,9 @@
-const path = require("path");
-
-const CWD = process.cwd();
+const path = require("path")
+const CWD = process.cwd()
 
 module.exports = {
   includePaths: [
     path.resolve(CWD, "node_modules/bootstrap/scss"),
-    path.resolve(CWD, "scss"),
-  ],
-};
+    path.resolve(CWD, "scss")
+  ]
+}
